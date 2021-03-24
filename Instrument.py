2943521e699ef02b6e061ad8f6e00860f2347417
Luka Nadiradze <lnadi17@freeuni.edu.ge>
2021-03-24 10:25:14 +0400

    Format strings with literal string interpolation

--- Instrument.py
+++ Instrument.py
@@ -15,13 +15,13 @@ class Instrument:
     def greet(self):
         idn = self.query("*IDN?")
         print(f"\nHello, I am: '{idn}'")
-        print(f'RsInstrument driver version: {self.instr.driver_version}')
-        print(f'Visa manufacturer: {self.instr.visa_manufacturer}')
-        print(f'Instrument full name: {self.instr.full_instrument_model_name}')
-        print(f'Instrument installed options: {",".join(self.instr.instrument_options)}')
+        print(f"RsInstrument driver version: {self.instr.driver_version}")
+        print(f"Visa manufacturer: {self.instr.visa_manufacturer}")
+        print(f"Instrument full name: {self.instr.full_instrument_model_name}")
+        print(f"Instrument installed options: {','.join(self.instr.instrument_options)}")
 
     def rename_trace(self, trace_id: int, trace_name: str):
-        self.write("configure:trace{}:name {}".format(trace_id, trace_name))
+        self.write(f"configure:trace{trace_id}:name {trace_name}")
 
     def reset(self):
         self.instr.reset()
@@ -30,8 +30,8 @@ class Instrument:
         return self.query("correction:state?")
 
     def set_display_mode(self, display_mode: str):
-        self.write("system:display:update {}".format(display_mode))
-        # instr.write_str("initiate1:continuous off")
+        self.write(f"system:display:update {display_mode}")
+        # self.write("initiate1:continuous off")
 
     def set_visa_timeout(self, timeout: int):
         self.instr.visa_timeout = timeout
@@ -40,28 +40,28 @@ class Instrument:
         self.instr.instrument_status_checking = status_checking
 
     def create_trace(self, channel: int, name: str, result: str):
-        self.write("calculate{}:parameter:sdefine '{}', '{}'".format(channel, name, result))
+        self.write(f"calculate{channel}:parameter:sdefine '{name}', '{result}'")
 
     def set_touchscreen_lock(self, ts_lock_mode: str):
-        self.write("system:tslock {}".format(ts_lock_mode))
+        self.write(f"system:tslock {ts_lock_mode}")
 
     def set_trace_points(self, channel: int, points: int):
-        self.write("sense{}:sweep:points {}".format(channel, points))
+        self.write(f"sense{channel}:sweep:points {points}")
 
     def copy_trace(self, memory_trace_name: str, data_trace_name: str):
-        self.write("trace:copy '{}', '{}'".format(memory_trace_name, data_trace_name))
+        self.write(f"trace:copy '{memory_trace_name}', '{data_trace_name}'")
 
     def assign_trace_to_window(self, window_id: int, trace_id: int, trace_name: str):
-        self.write("display:window{}:trace{}:feed '{}'".format(window_id, trace_id, trace_name))
+        self.write(f"display:window{window_id}:trace{trace_id}:feed '{trace_name}'")
 
     def create_window(self, window_id: int):
-        self.write("display:window{}:state on".format(window_id))
+        self.write(f"display:window{window_id}:state on")
 
     def remove_window(self, window_id: int):
-        self.write("display:window{}:state off".format(window_id))
+        self.write(f"display:window{window_id}:state off")
 
     def load_calibration_data(self, channel: int, file_name: str):
-        self.write("mmemory:load:correction {}, '{}'".format(channel, file_name))
+        self.write(f"mmemory:load:correction {channel}, '{file_name}'")
 
     def save_all_traces(self, channel: int, path: 'str',
                         formatted: bool = True,
@@ -69,11 +69,8 @@ class Instrument:
                         dec_separator: str = Types.DecimalSeparator.POINT,
                         field_separator: str = Types.FieldSeparator.SEMICOLON):
         formatted = 'formatted' if formatted else 'unformatted'
-        self.write("mmemory:store:trace:channel {}, '{}', {}, {}, {}, {}".format(channel, path,
-                                                                                 formatted,
-                                                                                 save_format,
-                                                                                 dec_separator,
-                                                                                 field_separator))
+        self.write(
+            f"mmemory:store:trace:channel {channel}, '{path}', {formatted}, {save_format}, {dec_separator}, {field_separator}")
 
     def close(self):
         self.instr.close()
